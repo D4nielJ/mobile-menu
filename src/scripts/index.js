232af2c83819e969337b6dce20234d28692997e4
d4nielj <d4niel.djm@gmail.com>
2021-10-01 23:07:06 -0500

    Add script to toggle menu

---
+++ src/scripts/index.js
@@ -0,0 +1,14 @@
+const menuIcons = document.querySelector('icon-container');
+const burguerIcon = document.querySelector('.burguer-icon');
+const closeIcon = document.querySelector('.x-icon');
+const navMenu = document.querySelector('.nav-menu');
+
+const toggleMenu = () => {
+  navMenu.classList.toggle('nav-menu--active');
+  burguerIcon.classList.toggle('burguer-icon--hidden');
+  closeIcon.classList.toggle('x-icon--hidden');
+};
+
+menuIcons.addEventListener('click', () => {
+  toggleMenu();
+});
